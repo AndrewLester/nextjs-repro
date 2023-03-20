0fc03f569a93bfa7ecb489829ed54bdc72dda005
AndrewLester <alester220@gmail.com>
2023-03-19 23:04:41 -0500

    Try something new

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,18 @@
 import '../styles/globals.css';
+import App from 'next/app';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+    return <Component {...pageProps} />;
 }
 
-MyApp.getInitialProps = async () => {
-  const { test } = await fetch(`${process.env.BASE_URL}/api/hello`).then(
-    (res) => res.json()
-  );
+MyApp.getInitialProps = async (ctx) => {
+    const { test } = await fetch(`${process.env.BASE_URL}/api/hello`).then(
+        (res) => res.json()
+    );
 
-  console.log('I need to use this...', test);
+    console.log("I'll use this...", test);
 
-  return {};
+    return App.getInitialProps(ctx);
 };
 
 export default MyApp;
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,4 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 export default (res) => {
-  'Error'.error(); // This is an accident, what can you do
-  res.status(200).json({ test: 'hi' });
+    'Error'.error(); // This is an accident, what can you do
+    res.status(200).json({ test: 'hi' });
 };
